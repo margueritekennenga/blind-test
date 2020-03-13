37ee7e249358793d596c987e2935e6c7784eef72
emelynemrg <emelynemrg@icloud.com>
2020-03-13 17:33:13 +0100

    quizz without sucess message

--- src/App.js
+++ src/App.js
@@ -1,86 +1,30 @@
 import React from 'react';
 import Quiz from './components/quiz'
-import logo from './logo.svg';
 import './App.css';
 import Themes from './components/theme';
 
-
 class App extends React.Component {
-
   constructor(props) {
     super(props)
-    this.state  = {
-    tracks : "", 
-    randomMusic: [],
-    response : '',
-    updateMusic: false,
+    this.state = {
     url : '',
-    start : false,
-  } 
-}
-
-getUrl = (data) =>{
-  this.setState({url:data});
-} 
-
-start = (data) =>{
-  this.setState({start:data});
-  this.componentDidMount()
-} 
-  
-componentDidMount(){ 
-    fetch(this.state.url)
-    .then(res => res.json())
-    .then(result => {
-      this.setState({
-        tracks: result.tracks.data,
-      })
-    }).then(()=>{
-      this.randomMusic()
-      this.resMusic()
-    })
- }
-
-updateMusic = (data) =>{
-  this.setState({
-    updateMusic: data
-  })
-    this.componentDidMount()
-}
-
-randomMusic(){
-  const randomMusic = [];
-  const showRandom = [];
-  const randomNumbers = []
-  let show = false 
-
-  for (let i = 0; i < 3; i++) {
-    const randomNumber = Math.floor(Math.random() * this.state.tracks.length);
-    if (randomNumbers.indexOf(randomNumber) === -1) { 
-      randomNumbers.push(randomNumber);
     }
-    randomMusic.push(this.state.tracks[randomNumbers[i]])
-    this.setState({randomMusic})
   }
-}
+  
 
-resMusic(){
-  const resMusic = Math.floor(Math.random() * 3);
-  this.setState({
-    response : this.state.randomMusic[resMusic],
-  })
-}
+  componentDidMount(){
+    const handles  = this.props.location.state.url
+    this.setState({
+        url : handles
+    })
+  }
 
-  render(){
+render(){
+  console.log(this.state.url)
     return (
       <div>
-        <Themes getUrl={this.getUrl} start={this.start} /> 
         <h1>Blindtest</h1>
-        <audio
-        autoPlay
-        src={this.state.response ? this.state.response.preview : ""}>
-        </audio>
-      {/* <Quiz updateMusic={this.updateMusic} tracks={this.state.randomMusic} response={this.state.response}/>  */}
+        <Quiz url={this.state.url} /> 
       </div>
     ); 
   }
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 let allAnswers = []
+let allCorrectAnswers = []
 
 class Quiz extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-    data : {},
+    show : [],
+    url : 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/1051470831',
+    tracks : "", 
+    randomMusic: [],
     question: '',
       answers : {
         1: '',
@@ -17,34 +21,75 @@ class Quiz extends Component {
       userAnswer: '',
       try : 0,
       point :0,
+      responses : "",
   }
   this.handleChange = this.handleChange.bind(this);
   this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    this.setState({
-        question: 'Qui interprète ce morceau ?'})
+    setTimeout(() => {
+      fetch(this.props.url)
+    .then(res => res.json())
+    .then(result => {
       this.setState({
-        answers : {
-          1: this.props.tracks[0].artist.name,
-          2: this.props.tracks[1].artist.name,
-          3: this.props.tracks[2].artist.name,
-      },
-      correct : this.props.response.artist.name
+        tracks: result.tracks.data,
       })
+    }).then(()=>{
+      this.randomMusic()
+      this.resMusic()
+      this.setQuizz()
+    })
+    }, 50);
   } 
+
+  randomMusic(){
+    const randomMusic = [];
+    const showRandom = [];
+    const randomNumbers = []
+  
+    for (let i = 0; i < 3; i++) {
+      const randomNumber = Math.floor(Math.random() * this.state.tracks.length);
+      if (randomNumbers.indexOf(randomNumber) === -1) { 
+        randomNumbers.push(randomNumber);
+      }
+      randomMusic.push(this.state.tracks[randomNumbers[i]])
+      this.setState({randomMusic})
+    }
+  }
+
+  resMusic(){
+    const resMusic = Math.floor(Math.random() * 3);
+    this.setState({
+      correct : this.state.randomMusic[resMusic],
+    })
+  }
+
+  setQuizz(){
+      this.setState({
+        question: 'Qui interprète ce morceau ?'})
+      setTimeout(() => {
+        this.setState({
+          answers : {
+            1: this.state.randomMusic[0].artist.name,
+            2: this.state.randomMusic[1].artist.name,
+            3: this.state.randomMusic[2].artist.name,
+        }
+        })
+      }, 10000);
+  }
 
   handleChange(event) {
     console.log(event.target.value)
     this.setState({userAnswer: event.target.value});
-  }
-
-  handleSubmit(event) {
-    if (this.state.try <= 2){
-      this.setState({try: +this.state.try+1});
+    console.log(this.state.userAnswer)
+    this.setState({
+      try: +this.state.try+1,
+      show : this.state.show.concat(this.state.correct) ,
+    }); 
+    if (this.state.try < 3 ){
       for(var i=0; i<3; i++){
-        if(this.state.userAnswer===this.state.correct){
+        if(event.target.value===this.state.correct.artist.name){
         console.log('good')
         this.setState({point: +this.state.point+1});
         }
@@ -52,38 +97,68 @@ class Quiz extends Component {
           console.log('bad')
         }
       } 
-    }
-    if (this.state.try < 2) {
-      event.preventDefault();   
-    this.props.updateMusic(true)
-    this.setState({
+    } 
+    else{
+      this.setState({
+        question: '',
       answers : {
         1: '',
         2: '',
         3: '',
       },
-      correct : '',
-    })
-    this.componentDidMount()
+      })
     }
+    if (this.state.try < 2 ) {
+      this.setState({
+        answers : {
+          1: '',
+          2: '',
+          3: '',
+      }
+      })
+      this.componentDidMount() 
     }
+  }
+
+  handleSubmit(event) {
+    this.props.history.push('/test')
+  }
 
   render(){
-
-    if (this.props.tracks.length === 0) {
+    if (this.state.tracks.length === 0) {
       return null
-    }else{
-      allAnswers = Object
-    .keys(this.state.answers)
-    .map(key => <div><label>{this.state.answers[key]}</label><input type="radio" onChange={this.handleChange} value={this.state.answers[key]} name='question' ></input></div>)
-    let points = this.state.point
+    } else{
+
+    allAnswers = Object
+      .keys(this.state.answers)
+      .map(key => <div><label for={this.state.answers[key]} >{this.state.answers[key]}<input  type="radio" value={this.state.answers[key]} name='question'onChange={this.handleChange} id={this.state.answers[key]} ></input></label></div>)
+     console.log(this.state.show)
+
+      allCorrectAnswers = Object 
+      .keys(this.state.show)
+      .map( (track) => 
+        <div className='correctAnswer'>
+        <img src={this.state.show[track].album.cover_medium}/>
+        <div className="correctAnswerInfos"><h2>{this.state.show[track].title}</h2>  <h3>{this.state.show[track].artist.name}</h3></div>
+        </div>
+      )   
+
+
     return(
       <div>
-        <p>{this.state.question}</p>
-        <div>
+        <p className='point'>{this.state.point}</p>
+        <div className='quizz'>
+        <audio
+        autoPlay
+        src={this.state.correct ? this.state.correct.preview : ""}>
+        </audio> 
+        <h2>{this.state.question}</h2>
+          <div className='answer'>
           {allAnswers}
-          <button onClick={this.handleSubmit}>Valider</button>
-          <h3>Good answers : {this.state.try > 2 ? points : ''}/3</h3>
+          </div>
+        </div>
+        <div className="allCorrectAnswers">
+         {allCorrectAnswers} 
         </div>
       </div>
     )
