f74abc30dcde6848f01483e6855a4b892bf15c92
Emelyne MARGUERITE KENNENGA <37291260+margueritekennenga@users.noreply.github.com>
2020-03-13 22:06:24 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import React from 'react';
-// import Music from './components/randomMusics'
 import Quiz from './components/quiz'
-import logo from './logo.svg';
 import './App.css';
-
+import Themes from './components/theme';
 
 class App extends React.Component {
-
-  state = {
-    tracks : "", 
-    randomMusic: []
-  }
-
-componentDidMount(){
-  fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/53362031')
-  .then(res => res.json())
-  .then(result => {
-    this.setState({
-      tracks: result.tracks.data,
-    })
-  }).then(()=>{
-    this.randomMusic()
-  })
-
-}
-
-randomMusic(){
-  const randomMusic = [];
-  const showRandom = [];
-  const randomNumbers = []
-  let show = false 
-
-  for (let i = 0; i < 3; i++) {
-    const randomNumber = Math.floor(Math.random() * this.state.tracks.length);
-    if (randomNumbers.indexOf(randomNumber) === -1) { 
-      randomNumbers.push(randomNumber);
+  constructor(props) {
+    super(props)
+    this.state = {
+    url : '',
     }
-    randomMusic.push(this.state.tracks[randomNumbers[i]])
-    this.setState({randomMusic})
   }
-}
+  
 
-  render(){
+  componentDidMount(){
+    const handles  = this.props.location.state.url
+    this.setState({
+        url : handles
+    })
+  }
+
+render(){
+  console.log(this.state.url)
     return (
       <div>
         <h1>Blindtest</h1>
-        <p>{this.state.randomMusic.length  >= 3 ? this.state.randomMusic[0].title : ""}</p>
-      <Quiz tracks={this.state.srandomMusic}/> 
+        <Quiz url={this.state.url} /> 
       </div>
     ); 
   }
